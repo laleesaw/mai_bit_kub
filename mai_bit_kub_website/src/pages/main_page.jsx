f5f12abc45deaa8037535efab52a63b98444b5b6
Chinnabanchorn Yuenman <s6501012630051@email.kmutnb.ac.th>
2025-09-20 00:18:33 +0700

    to day I nud dek, I tum toa ni por.

--- mai_bit_kub_website/src/pages/main_page.jsx
+++ mai_bit_kub_website/src/pages/main_page.jsx
@@ -1,12 +1,29 @@
 import "./main_page.css"
+import Profile from "../assets/profile_icon.png"
+
+function box_content(width_ratio, height_ration){
+    return(
+        <div class = "box">
+            
+        </div>
+    );
+}
 
 function main_page(){
     return(
         <div class = "main_monitor">
-            <div class = "profile"></div>
-            <div class = "activity"></div>
-            <div class = "friends"></div>
-            <div class = "available"></div>
+            <div class = "top">            
+                <div class = "profile">
+                    <img id = "profile_icon" src = {Profile}></img>
+                    <div class = "username">USERNAME</div>
+                </div>
+                <div class = "activity"></div>
+            </div>
+            <div class = "bottom">
+                <div class = "friends"></div>
+                <div class = "available"></div>
+            </div>
+
         </div>
     );
 }
