4d7ea7d266a98d6b8d3722696bdbeaa2bdad9ec7
tawangimjiz <tawangimji@hotmail.com>
2025-10-13 22:30:11 +0700

    update SignIn and SignUp

--- mai_bit_kub_website/src/app.jsx
+++ mai_bit_kub_website/src/app.jsx
@@ -1,36 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Head/Header.jsx";
-import Home from './pages/Home.jsx'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Main_page from './pages/main_page.jsx'
-
-
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
   return (
     <Router>
-      <Header></Header>
-      <nav>
-        <Link to="/Home"></Link>
-        <Link to="/SignIn"></Link>
-        <Link to="/SignUp"></Link>
-        <Link to="/Main_page"></Link>
-        
-
-      </nav>
-
+      <Header />
       <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Main_page" element={<Main_page />} />
-
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
--- mai_bit_kub_website/src/pages/SignIn.jsx
+++ mai_bit_kub_website/src/pages/SignIn.jsx
@@ -1,8 +1,43 @@
-import "./SignIn.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css";
+import logo from "../assets/logo_mai_bit_kub.png";
 
-function SignIn(){
-    return(
-        <div></div>
+function SignIn() {
+    const [remember, setRemember] = useState(false);
+    return (
+        <div className="sign-bg">
+            <div className="sign-container fade-in">
+                <div className="sign-left">
+                      <img src={logo} alt="Mai Bit Kub Logo" className="sign-logo" />
+                      <p className="sign-desc">Mai bit kub helps you connect with your friend</p>
+                </div>
+                <div className="sign-right">
+                    <h2 className="sign-form-title">Sign in to MAi Bit Kub</h2>
+                    <form className="sign-form">
+                        <input type="text" placeholder="USERNAME" className="sign-input" />
+                        <input type="password" placeholder="PASSWORD" className="sign-input" />
+                        <div className="sign-remember">
+                            <span>REMEMBER</span>
+                            <span
+                                className={`toggle-remember${remember ? " active" : ""}`}
+                                onClick={() => setRemember(!remember)}
+                                tabIndex={0}
+                                role="button"
+                                aria-pressed={remember}
+                            >
+                                <span className="toggle-circle" />
+                            </span>
+                        </div>
+                        <button type="submit" className="sign-btn login-btn">Login</button>
+                    </form>
+                    <div className="sign-bottom-text">
+                        DON’T HAVE AN ACCOUNT? <Link to="/signup" className="sign-link">SIGN UP</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
+
 export default SignIn;
