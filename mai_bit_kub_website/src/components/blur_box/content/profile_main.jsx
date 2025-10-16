b10a8b6180a69a15479231e7879f814cc031c75f
tawangimjiz <s6501012630115@email.kmutnb.ac.th>
2025-10-16 20:52:01 +0700

    update main_page

--- mai_bit_kub_website/src/components/blur_box/content/profile_main.jsx
+++ mai_bit_kub_website/src/components/blur_box/content/profile_main.jsx
@@ -1,10 +1,10 @@
 import "./profile_main.css"
-import Profile_icon from "../../../assets/profile_icon.png"
+import Profile_main from "../../../assets/profile_icon_main.png"
 
 function Profile(){
     return(
         <div className = "profile">
-            <img src = {Profile_icon}></img>
+            <img src = {Profile_main}></img>
             <div className = "username">USERNAME</div>
         </div>
     );
--- mai_bit_kub_website/src/pages/main_page.jsx
+++ mai_bit_kub_website/src/pages/main_page.jsx
@@ -1,8 +1,10 @@
 import "./main_page.css"
-import Profile from "../assets/profile_icon.png"
 import Blur_box from "../components/blur_box/blur_box.jsx"
 import Profile_content from "../components/blur_box/content/profile_main.jsx"
 import Activity_content from "../components/blur_box/content/activity_main.jsx"
+import Friend_content from "../components/blur_box/content/friend_main.jsx"
+import Avai_content from "../components/blur_box/content/available_main.jsx"
+
 
 
 // function main_page(){
@@ -28,12 +30,22 @@ import Activity_content from "../components/blur_box/content/activity_main.jsx"
 function main_page(){
     return(
         <div class = "box">
-            <Blur_box width = {2} height = {2}>
-                <Profile_content></Profile_content>
-            </Blur_box>
-            <Blur_box width = {2} height = {2}>
-                <Activity_content></Activity_content>
-            </Blur_box>
+            <div className = "top">
+                <Blur_box width = {2} height = {2}>
+                    <Profile_content></Profile_content>
+                </Blur_box>
+                <Blur_box width = {2} height = {2}>
+                    <Activity_content></Activity_content>
+                </Blur_box>
+            </div>
+            <div className = "bottom">
+                <Blur_box width = {8} height = {2}>
+                    <Friend_content></Friend_content>
+                </Blur_box>
+                <Blur_box width = {1.145} height = {2}>
+                    <Avai_content></Avai_content>
+                </Blur_box>
+            </div>
         </div>
     );
 }
